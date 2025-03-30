6bbc572edf34742e810a42907cc691e5109a1995
darlxxvii <jekenn9@gmail.com>
2025-03-30 16:56:06 +0500

    did lab08

---
+++ lab08/shop_back/api/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .models import Product, Category
+from .serializers import ProductSerializer, CategorySerializer
+
+class ProductListView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class CategoryListView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = CategorySerializer
+
+class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = CategorySerializer
+
+class CategoryProductsView(generics.ListAPIView):
+    serializer_class = ProductSerializer
+
+    def get_querySet(self):
+        category_id = self.kwargs['id']
+        return Product.objects.filter(category_id=category_id)
+
+    
